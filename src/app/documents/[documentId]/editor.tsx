a025a1ac990164fc04c0b6e14a42a838496ada2e
Tan Cao <tancaodev.github@gmail.com>
2025-04-10 11:09:44 +0700

    feat: align button, list button

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import StarterKit from '@tiptap/starter-kit'
+import TextAlign from '@tiptap/extension-text-align'
 import Link from '@tiptap/extension-link'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
@@ -55,6 +56,9 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            TextAlign.configure({
+                types: ['heading', 'paragraph']
+            }),
             Link.configure({
                 openOnClick: false,
                 autolink: true,
