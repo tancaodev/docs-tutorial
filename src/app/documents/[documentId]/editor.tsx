6561f02063778b8830b304b33a2546f46ea54dec
Tan Cao <tancaodev.github@gmail.com>
2025-04-10 12:21:35 +0700

    feat: font size tool

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import TableRow from '@tiptap/extension-table-row'
 import { useEditor, EditorContent } from '@tiptap/react'
 
 import { useEditorStore } from '@/store/use-editor-store'
+import { FontSizeExtension } from '@/extensions/font-size'
 
 export const Editor = () => {
     const { setEditor } = useEditorStore()
@@ -56,6 +57,7 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            FontSizeExtension,
             TextAlign.configure({
                 types: ['heading', 'paragraph']
             }),
