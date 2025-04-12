e02809e4f068ab50a6bd9009e7bc0a9725cd3713
Tan Cao <tancaodev.github@gmail.com>
2025-04-13 04:59:39 +0700

    feat: template gallery component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import { Navbar } from './navbar'
+import { TemplatesGallery } from './templates-gallery'
 
 const Home = () => {
     return (
@@ -8,11 +8,7 @@ const Home = () => {
                 <Navbar />
             </div>
             <div className='mt-16'>
-                Click{' '}
-                <Link href='/documents/123'>
-                    <span className='text-blue-500 underline'>&nbsp;here&nbsp;</span>
-                </Link>{' '}
-                to go to document id
+                <TemplatesGallery />
             </div>
         </div>
     )
